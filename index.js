68e770aa44c08b91076ca343682c04991b234815
Tamoy <sinclairtamoy@gmail.com>
2022-04-27 00:09:43 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    return customerName = "BOB"
+}
+
+var bestCustomer = undefined
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(x) {
+    return bestCustomer = x;
+}
+
+const leastFavoriteCustomer = "Barbara"
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "Barbara";
+
+}
